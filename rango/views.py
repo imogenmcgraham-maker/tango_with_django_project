37eeb6525aac61e19425c0151fb160104083f5ed
Imogen Graham <2860808G@student.gla.ac.uk>
2026-02-13 13:40:49 +0000

    chapter 10 finished/exersize incomplet(solution incorrect)

--- rango/views.py
+++ rango/views.py
@@ -12,6 +12,7 @@ from rango.forms import PageForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
 
 from rango.forms import UserForm, UserProfileForm
 
@@ -30,15 +31,24 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages']=top_pages
+    context_dict['visits'] = int(request.COOKIES.get('visits', '0'))
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
 
     # Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/about.html',)
+    #cookie p183 chapter 10
+    context_dict = {}
+    context_dict['visits'] = int(request.COOKIES.get('visits', '0'))
+
+    response = render(request, 'rango/about.html', context=context_dict)
+    visitor_cookie_handler(request, response)
+
+    # Render the response and send it back!
+    return response
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -230,3 +240,25 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
+
+
+def visitor_cookie_handler(request, response):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+        '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if True:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        # Set the last visit cookie
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    # Update/set the visits cookie
+    response.set_cookie('visits', visits)
