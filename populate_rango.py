9c1d2620816b9bf0b1dcebd886e4c3c22425b28d
Imogen Graham <2860808G@student.gla.ac.uk>
2026-02-13 10:07:52 +0000

    chapter 6 done (untested)

--- populate_rango.py
+++ populate_rango.py
@@ -46,7 +46,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], views=88)
 
     # Print out the categories we have added.
     for c in Category.objects.all():
@@ -60,7 +60,7 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name, views=0, likes=0):
+def add_cat(name, views=8, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
     c.views = views
     c.likes = likes
